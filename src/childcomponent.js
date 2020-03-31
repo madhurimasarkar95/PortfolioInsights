33033f5b77e049af3091d3592fc5bfbddb5dc052
madhurimasarkar95 <47918928+madhurimasarkar95@users.noreply.github.com>
2020-03-31 16:20:40 +0600

    updated
    updated

--- src/childcomponent.js
+++ src/childcomponent.js
@@ -12,11 +12,12 @@ class Child extends Component{
             <div style={{position:'absolute',top: '0px',right :'250px',width:'50%', height:'400px', backgroundColor:'#D3D3D3' ,border: '3px solid #73AD21'}}>
                       <p>FirstName:{this.props.parentstate.FirstName} </p>
                       <p>LastName:{this.props.parentstate.LastName} </p>
-                      <p>DateOfBirth:{this.props.parentstate.DateOfBirth} </p>
+                      <p>DateOfBirth:{this.props.parentstate.DateOfBirth.toString()} </p>
                       <p>Email: {this.props.parentstate.Email} </p>
                       <p>PhoneNumber: {this.props.parentstate.PhoneNumber} </p>
                       <p>Football:{this.props.parentstate.Football.toString()} </p>
                       <p>Cricket:{this.props.parentstate.Cricket.toString()} </p>
+                      <p>Location:{this.props.parentstate.selectValue}</p>
                       <p>Gender:{this.props.parentstate.Gender} </p>
                       <p>Message:{this.props.parentstate.Message} </p>
             </div>
